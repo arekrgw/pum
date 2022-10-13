446bfcb3738bd352919331da7962f49ee488cfdc
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2022-10-13 16:37:02 +0200

    basic functionality

--- lab1b/main.js
+++ lab1b/main.js
@@ -10,19 +10,67 @@ function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-let lastColor = "green";
+const redVal = 0;
+const greenVal = 128;
+let current = 0;
 
-function paintCircle(color) {
+let toGreen = true;
+
+function paintCircle(color, x, y) {
   ctx.beginPath();
   ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
   ctx.fillStyle = color;
   ctx.fill();
 }
 
-paintCircle(lastColor);
+paintCircle(`hsl(${current}, 100%, 50%)`);
 
 setInterval(() => {
-  const currentColor = lastColor === "green" ? "blue" : "green";
+  if (toGreen) {
+    current += 1;
+    if (current >= 128) {
+      toGreen = false;
+    }
+  } else {
+    current -= 1;
+    if (current <= 0) {
+      toGreen = true;
+    }
+  }
+
+  console.log(current)
+  const currentColor = `hsl(${current}, 100%, 50%)`;
   paintCircle(currentColor);
-  lastColor = currentColor;
-}, 1000);
+}, 20);
+
+// first circle
+let colorF = "green";
+let updateTime = 1000;
+let x = 70, y = 70;
+
+
+function update(timeElapsed) {
+  // first circle
+  if(timeElapsed > updateTime) {
+    colorF === "green" ? "blue" : "green";
+  }
+
+}
+
+function render() {
+  // first circle
+  paintCircle(colorF, x, y);
+}
+
+let lastRender = 0;
+function draw(timestamp) {
+  const progress = timestamp - lastRender;
+
+  update(progress);
+  render();
+
+  lastRender = timestamp;
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw)
