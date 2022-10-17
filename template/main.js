9024d674589231abbd829e3a9fe8b082cb64fb1f
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2022-10-17 19:47:59 +0200

    prepared template

---
+++ template/main.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const width = canvas.width;
+const height = canvas.height;
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function update() {}
+
+function render() {}
+
+let lastRender;
+function draw(timestamp) {
+  if (lastRender && timestamp - lastRender < 1) {
+    requestAnimationFrame(draw);
+    return;
+  }
+
+  lastRender = timestamp;
+  update(timestamp);
+  render();
+
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
