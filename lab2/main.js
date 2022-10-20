8f575183fd0cd82556d1966e4ed658589a3ebb7a
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2022-10-20 16:53:25 +0200

    rendering stairs

--- lab2/main.js
+++ lab2/main.js
@@ -33,19 +33,68 @@ class Floor {
 }
 
 const floor = new Floor();
+
+const StairConstants = {
+  height: 50,
+  width: 100,
+  stairsToRender: 0,
+  lastUpdated: 0,
+  maxStairs: 5,
+  checkStairsToRender(timestamp) {
+    if (
+      timestamp - this.lastUpdated > 1000 &&
+      this.stairsToRender < this.maxStairs
+    ) {
+      this.lastUpdated = timestamp;
+      this.stairsToRender++;
+    }
+  },
+};
 class Stair {
+  color = "brown";
+  height = StairConstants.height;
+  width = StairConstants.width;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
   draw() {
     // draw stairs on canvas
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
   }
 }
 
-function update() {}
+const beginX = 150;
+const beginY = height - (StairConstants.height + 50);
+
+const stairs = new Array(5)
+  .fill(0)
+  .map(
+    (_, i) =>
+      new Stair(
+        beginX + i * StairConstants.width,
+        beginY - i * StairConstants.height
+      )
+  );
+
+let lastRender;
+function update(timestamp) {
+  StairConstants.checkStairsToRender(timestamp);
+}
 
 function render() {
   floor.draw();
+  stairs
+    .slice()
+    .slice(0, StairConstants.stairsToRender + 1)
+    .forEach((stair) => stair.draw());
 }
 
-let lastRender;
 function draw(timestamp) {
   if (lastRender && timestamp - lastRender < 1) {
     requestAnimationFrame(draw);
