1b617bd27cbe4b145b01ced34bb10d648c0a6536
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2022-10-26 20:06:41 +0200

    collisions update

--- template/drawing.js
+++ template/drawing.js
@@ -6,7 +6,7 @@ const centerY = canvas.height / 2;
 const width = canvas.width;
 const height = canvas.height;
 
-function colliderBallRect(circle, rect) {
+function collCR(circle, rect) {
   const distX = Math.abs(circle.x - rect.x - rect.width / 2);
   const distY = Math.abs(circle.y - rect.y - rect.height / 2);
 
@@ -29,13 +29,26 @@ function colliderBallRect(circle, rect) {
   return dx * dx + dy * dy <= circle.radius * circle.radius;
 }
 
+function collRR() {
+  if (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.h + rect1.y > rect2.y
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
 const actors = [];
 class Actor {
   constructor(name) {
     this.name = name;
   }
-  get actors() {
-    return actors.filter((actor) => actor !== this);
+  actors(name = null) {
+    return actors.filter((actor) => actor !== this && name !== actor.name);
   }
   draw(timestamp) {}
   update(timestamp) {}
